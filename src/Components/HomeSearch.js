dfd4037a3d530f059ef88c382ac0e03607a66973
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-04 18:46:37 -0500

    search bar returning data

--- src/Components/HomeSearch.js
+++ src/Components/HomeSearch.js
@@ -1,5 +1,5 @@
 import { API_URL } from '../API';
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect, Component} from 'react';
 // import axios from 'axios'
 import bookImg from '../svg/undraw_Book_reading_re_fu2c.png';
 import SearchArea from './SearchArea';
@@ -9,8 +9,8 @@ import request from 'superagent';
 import '../Home.css'
 
 
-export default function HomeSearch() {
-    constructor(props){
+class HomeSearch extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             books: [],
@@ -22,8 +22,8 @@ export default function HomeSearch() {
         e.preventDefault();
         request
             .get("https://www.googleapis.com/books/v1/volumes")
-            .query({ q: this.searchField })
-            .then(() => {
+            .query({ q: this.state.searchField })
+            .then((data) => {
                 console.log(data);
             })
     }
@@ -31,14 +31,17 @@ export default function HomeSearch() {
     handleSearch = (e) => {
         this.setState({ searchField: e.target.value })
     }
-
-return (
-    <div className="home-search">
-        <img class='image' src={bookImg} alt="Book image"/>
-        <h1 className='title'>Bookworm</h1>
-        <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch}/>
-        <button>Surprise me!</button>
-        <h3>Library in your pocket, Search your favorite books or find something new!</h3>
-     </div>
-)
+render() {
+    return (
+        <div className="home-search">
+            <img className='image' src={bookImg} alt="Book image"/>
+            <h1 className='title'>Bookworm</h1>
+            <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch}/>
+            <button>Surprise me!</button>
+            <h3>Library in your pocket, Search your favorite books or find something new!</h3>
+         </div>
+    )
 }
+}
+
+export default HomeSearch;
