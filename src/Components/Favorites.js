e414740936912ac79017cb957f824502a4ee601a
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-05 18:50:45 -0500

    favorites functionality update

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,11 +1,46 @@
 import React from 'react'
 import "../App.css"
-
+import { useAppContext } from './context/app.context'
 
 
 const Favorites = () => {
+
+    const {favorites, addToFavorites, removeFromFavorites} = useAppContext();
+
+
+    const favoritesChecker = (id) => {
+        const boolean = favorites.some((book) => book.id === id);
+        return boolean;
+    }
+
+    
     return (
-        <div>Favorites</div>
+        <div className="favorites">
+            {favorites.length > 0 ? favorites.map((volumeInfo) => (
+                <div key={volumeInfo.id} className="book-card">
+                    <div>
+                        <h4>{volumeInfo.title}</h4>
+                    </div>
+                    <div>
+                        <h5>{volumeInfo.authors}</h5>
+                    </div>
+                    <div>
+                        <img src={volumeInfo.imageLinks} alt="#"/>
+                    </div>
+                    <div>
+                        {favoritesChecker(book.id) ? (
+                            <button onClick={()=> removeFromFavorites(book.id)}>Remove From Favorites</button>
+                        ) : (
+                            <button onClick={()=> addToFavorites(book)}>Add to Favorites</button>
+                        )
+                    }
+                        
+                    </div>
+
+                </div>
+            )): 
+                <h1>You Do Not Have any Favorites Yet</h1>}
+        </div>
     )
 }
 
