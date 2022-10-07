e95151819be89118ff6990836eedede1bda98273
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-06 21:20:13 -0500

    working through display funcionality

--- src/Components/DisplayBook.js
+++ src/Components/DisplayBook.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams } from 'react-router-dom'
+import axios from 'axios'
 import '../Home.css'
 
 
 
-export default function DisplayBook() {
+export default function DisplayBook(props) {
+
+    const [book, setBook] = useState({
+            image={book.volumeInfo.imageLinks.thumbnail},
+            title={book.volumeInfo.title},
+            author={book.volumeInfo.authors},
+            published={book.volumeInfo.publishedDate},
+            id={book.id},
+            description={book.volumeInfo.description}
+});
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`)
+        .then(res => {
+            setBook(res.data)
+        }).catch(err=> console.log(err))
+    },[id])
 
     return (
-        <div>displaybook</div>
+        <div className="display-book">
+            <img src={book.volumeInfo.image} alt=""/>
+            <div className="desc">
+                <h3>{book.volumeInfo.title}</h3>
+                <h5>Author: {book.volumeInfo.author}</h5>
+                <p>Published {book.volumeInfo.published}</p>
+                <p>Description: {book.volumeInfo.description}</p>
+                {/* <div>
+                        {favoritesChecker(book.id) ? (
+                            <button onClick={()=> removeFromFavorites(book.id)}>Remove From Favorites</button>
+                        ) : (
+                            <button onClick={()=> addToFavorites(book)}>Add to Favorites</button>
+                        )
+                    }
+                </div> */}
+            </div>
+        </div>
     )
 }
--- src/Components/SearchedBooks.js
+++ src/Components/SearchedBooks.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import BookCard from './BookCard'
 
+import '../Home.css'
+
 
 
 const SearchedBooks = (props) => {
@@ -8,12 +10,14 @@ const SearchedBooks = (props) => {
         <div className="list">
             {
                 props.books.map((book, i) => {
+                    console.log("book",book)
                    return <BookCard 
                             key={i}
                             image={book.volumeInfo.imageLinks.thumbnail}
                             title={book.volumeInfo.title}
                             author={book.volumeInfo.authors}
                             published={book.volumeInfo.publishedDate}
+                            id={book.id}
                             /> 
                 })
             }
