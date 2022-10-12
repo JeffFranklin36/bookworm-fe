10fae0c409295322c1389a1d6e21f2a27b794da2
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-11 19:10:19 -0500

    favorites component added

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 // import { useAppContext } from './context/app.context'
 import axios from 'axios'
+import Favorites from './Favorites'
+
 
 import '../Home.css'
 
@@ -8,44 +10,13 @@ import '../Home.css'
 
 const BookCard = (props) => {
 
-    // const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-    // useEffect(() => {
-    //     return () => saveBookIds(savedBookIds);
-    //   });
-
-    // const handleSaveBook = async (bookId) => {
-    //     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    //     const token = Auth.loggedIn() ? Auth.getToken() : null;
     
-    //     if (!token) {
-    //       return false;
-    //     }
-    
-    //     try {
-    //       const { data } = await saveBook({
-    //         variables: { newBook: { ...bookToSave } },
-    //       });
-    
-    //       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    //     } catch (err) {
-    //       console.error(err);
-    //     }
-    //   };
 
     return (
         <div className="card-container">
             <img src={props.image} alt="" />
             <div className="desc">
-            <button className="save-button">
-                Save button
-            {/* disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                      className='btn-block btn-info'
-                      onClick={() => handleSaveBook(book.bookId)}>
-                      {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                        ? 'This book has already been saved!'
-                        : 'Save this Book!'} */}
-            </button>
+            <Favorites />
                 <h3>{props.title}</h3>
                 <h5>Author: {props.author}</h5>
                 <p>Published Date: {props.published === '0000' ? 'Not Available' : props.published.substring(0, 4)}</p>
